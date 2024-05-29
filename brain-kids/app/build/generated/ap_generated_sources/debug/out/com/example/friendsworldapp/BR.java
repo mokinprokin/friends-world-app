7c6d83080e6fcba60b1f9b4de3ca37b37d200ffe
Макар Павлович <106832520+mokinprokin@users.noreply.github.com>
2024-05-29 12:18:22 +0600

    Add files via upload

---
+++ brain-kids/app/build/generated/ap_generated_sources/debug/out/com/example/friendsworldapp/BR.java
@@ -0,0 +1,5 @@
+package com.example.friendsworldapp;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ brain-kids/app/build/generated/data_binding_trigger/debug/com/example/friendsworldapp/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.friendsworldapp;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
